5970878dea8f96419fb5feec9a71cc06d50fdc54
ShivamVarshney <uietcse1923shivamvarshney@kuk.ac.in>
2023-08-04 13:58:54 +0530

    FindDuplicates

---
+++ arrays/10findDuplicates.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+  public:
+    vector<int> duplicates(int arr[], int n) {
+        vector<int> ans;
+        for(int i = 0 ; i < n ; i++){
+            int curr = arr[i]%n;
+            arr[curr] += n;
+        }
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] >= 2*n)
+                ans.push_back(i);
+            arr[i] %= n;
+        }
+        if(ans.size() == 0) return {-1};
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        Solution obj;
+        vector<int> ans = obj.duplicates(a, n);
+        for (int i : ans) cout << i << ' ';
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
